932a8c8a922172cd90170df9c8df540086719077
Айдер <emage.haf@gmail.com>
2022-02-19 19:57:02 +0200

    Created skeleton for microservices

---
+++ Backend/DataApi/src/main/java/com/microservices/dataapi/DataApiApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.dataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataApiApplication.class, args);
+    }
+
+}
