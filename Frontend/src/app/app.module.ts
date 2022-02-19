f474528e67929bb2344c2b079991a248f931ca0d
Tetyana Yahodska <yahodska@gmail.com>
2022-02-19 15:18:41 +0200

    Created Login Form and Two factor autentication Form components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { TwoFactorAuthenticationFormComponent } from './two-factor-authentication-form/two-factor-authentication-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginFormComponent,
+    TwoFactorAuthenticationFormComponent
   ],
   imports: [
     BrowserModule,
