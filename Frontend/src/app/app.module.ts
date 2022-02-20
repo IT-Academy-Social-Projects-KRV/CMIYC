a6fe3b432a26b2a38f18a7c3a1f3cadcf262bca0
PavelHorishny <horishnypavel@gmail.com>
2022-02-20 11:43:19 +0200

    error pages components and their desighn were added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { Error403Component } from './errorpages/error403/error403.component';
+import { Error404Component } from './errorpages/error404/error404.component';
+import { Error500Component } from './errorpages/error500/error500.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    Error403Component,
+    Error404Component,
+    Error500Component
   ],
   imports: [
     BrowserModule,
