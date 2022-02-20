6f5d97e801f3dc8af3f392a25bc097a7df264c45
PavelHorishny <horishnypavel@gmail.com>
2022-02-20 22:53:28 +0200

    changed some routes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AdminManageSchemaListComponent} from "./admin-manage-schema-list/admin-manage-schema-list.component";
-import {AdminCreateSchemaFormComponent} from "./admin-create-schema-form/admin-create-schema-form.component";
 import {AdminManageUserListComponent} from "./admin-manage-user-list/admin-manage-user-list.component";
 import {RegisterComponent} from "./register/register.component";
 import {LoginFormComponent} from "./login-form/login-form.component";
-import {
-  TwoFactorAuthenticationFormComponent
-} from "./two-factor-authentication-form/two-factor-authentication-form.component";
 import {Error403Component} from "./errorpages/error403/error403.component";
 import {Error404Component} from "./errorpages/error404/error404.component";
 import {Error500Component} from "./errorpages/error500/error500.component";
@@ -19,7 +15,6 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'admin/allSchemas', component: AdminManageSchemaListComponent },
   { path: 'admin/allUsers', component: AdminManageUserListComponent },
-  { path: "register", component: RegisterComponent },
   { path: "login", component: LoginFormComponent },
   { path: "twoFactor", component: TwoFactorAuthenticationFormComponent },
   { path: "error403", component: Error403Component},
