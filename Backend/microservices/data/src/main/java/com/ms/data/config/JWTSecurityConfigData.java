b46ba3f5c3792aa7c353a8a9152cf8a5081f4975
dkhomyk <khomyk.dima42@gmail.com>
2022-03-28 16:38:09 +0300

    edit JWTSecurityConfigData.class

--- Backend/microservices/data/src/main/java/com/ms/data/config/JWTSecurityConfigData.java
+++ Backend/microservices/data/src/main/java/com/ms/data/config/JWTSecurityConfigData.java
@@ -10,7 +10,7 @@ public class JWTSecurityConfigData extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authz -> authz
-                        .antMatchers(HttpMethod.GET, "/**").permitAll()
+                        .antMatchers(HttpMethod.GET, "/**").hasAuthority("SCOPE_user")
                         .antMatchers(HttpMethod.POST, "/**").hasAuthority("SCOPE_user")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt());
