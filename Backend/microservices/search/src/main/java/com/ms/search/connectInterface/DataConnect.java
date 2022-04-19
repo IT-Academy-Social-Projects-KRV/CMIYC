fc39a66fbb5988444e47d016c38ec3cb4aad90ef
Camper689 <camper689@gmail.com>
2022-04-19 14:47:50 +0300

    Added response caching on Search API

--- Backend/microservices/search/src/main/java/com/ms/search/connectInterface/DataConnect.java
+++ Backend/microservices/search/src/main/java/com/ms/search/connectInterface/DataConnect.java
@@ -3,7 +3,6 @@ package com.ms.search.connectInterface;
 import com.ms.search.config.CaffeineConfig;
 import com.ms.search.model.XmlObject;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,10 +11,9 @@ import javax.xml.bind.JAXBException;
 import java.util.List;
 
 @FeignClient(name = "data")
-//@CacheConfig(cacheNames = CaffeineConfig.CACHE_SCHEMAS)
+@CacheConfig(cacheNames = CaffeineConfig.CACHE_SCHEMAS)
 public interface DataConnect {
 
     @GetMapping("/schemas")
-    @Cacheable(CaffeineConfig.CACHE_SCHEMAS)
-    List<XmlObject> xmlSchema(@RequestHeader(value = "Authorization", required = true) String authorizationHeader) throws JAXBException;
+    List<XmlObject> xmlSchema(@RequestHeader(value = "Authorization") String authorizationHeader) throws JAXBException;
 }
