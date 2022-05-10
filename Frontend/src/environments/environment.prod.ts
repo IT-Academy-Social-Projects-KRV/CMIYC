62232e4bde035873b81cdd6ee142fdff656d94ae
ZX-pool <igor.shrol@gmail.com>
2022-05-10 20:43:40 +0300

    proxy-pass

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authServer: "http://authority.ms",
+  authServer: "http://authority",
   dataAPI: "http://dataapi:8081",
   searchAPI: "http://searchapi:8080"
 };
