e86e74bd42b2a12dd657033c6c99c0ddb3eb1c2c
ZX-pool <igor.shrol@gmail.com>
2022-05-10 19:21:20 +0300

    authority.ms

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authServer: "http://authority:8090",
+  authServer: "http://authority.ms",
   dataAPI: "http://dataapi:8081",
   searchAPI: "http://searchapi:8080"
 };
