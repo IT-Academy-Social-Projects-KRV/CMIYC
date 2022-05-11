193a06af427b078ee352fdd550a72a061b4dbc35
dkhomyk <khomyk.dima42@gmail.com>
2022-05-12 00:31:58 +0300

    added custom starter for cors filter

--- Backend/custom-starter/src/main/java/com.customstarter/config/CorsFilter.java
+++ Backend/custom-starter/src/main/java/com.customstarter/config/CorsFilter.java
@@ -1,19 +1,24 @@
-package com.ms.search.config;
+package com.customstarter.config;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+
+
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
-
+@PostConstruct
+public void hello(){
+    System.out.println("hello");
+}
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
