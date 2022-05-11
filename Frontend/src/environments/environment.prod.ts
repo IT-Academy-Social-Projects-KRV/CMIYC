9f88972ebbb12d2aef4245220726a9f738ea605f
ZX-pool <igor.shrol@gmail.com>
2022-05-11 21:08:51 +0300

    hard

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authServer: "${AUTH_URL}",
+  authServer: "http://34.116.181.83:8090",
   dataAPI: "${DATA_URL}",
   searchAPI: "${SEARCH_URL}"
 };
