568cc1f980f1da3ff287158ede821a02b099b5fe
ZX-pool <igor.shrol@gmail.com>
2022-05-11 20:43:25 +0300

    env.prod

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authServer: "http://34.107.146.133/authority",
-  dataAPI: "http://dataapi:8081",
-  searchAPI: "http://searchapi:8080"
+  authServer: "${AUTH_URL}",
+  dataAPI: "${DATA_URL}",
+  searchAPI: "${SEARCH_URL}"
 };
