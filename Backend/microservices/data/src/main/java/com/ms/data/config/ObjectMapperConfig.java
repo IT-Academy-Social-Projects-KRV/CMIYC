22c16e9f9934d9faa25011920b64093b8a2a908a
Camper689 <camper689@gmail.com>
2022-05-12 19:47:45 +0300

    Fixed error in object mapper config

--- Backend/microservices/data/src/main/java/com/ms/data/config/ObjectMapperConfig.java
+++ Backend/microservices/data/src/main/java/com/ms/data/config/ObjectMapperConfig.java
@@ -2,17 +2,17 @@ package com.ms.data.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import javax.annotation.PostConstruct;
+
+@Component
 public class ObjectMapperConfig {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public ObjectMapperConfig(@Autowired ObjectMapper objectMapper) {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        return objectMapper;
     }
 }
