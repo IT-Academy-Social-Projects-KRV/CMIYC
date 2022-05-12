dc1f71968a29eef9dd9042d9d2bc6dea6e447d95
dkhomyk <khomyk.dima42@gmail.com>
2022-05-12 11:30:32 +0300

    delete postconstruct annotation

--- Backend/custom-starter/src/main/java/com.customstarter/config/CorsFilter.java
+++ Backend/custom-starter/src/main/java/com.customstarter/config/CorsFilter.java
@@ -3,22 +3,23 @@ package com.customstarter.config;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
-@PostConstruct
-public void hello(){
-    System.out.println("hello");
-}
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
