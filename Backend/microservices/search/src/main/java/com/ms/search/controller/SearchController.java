caa02b6a38b5bb8d7deb41cf1980fd6bab9f79af
Айдер <emage.haf@gmail.com>
2022-05-16 00:32:17 +0300

    Show all history search done, but we need separate by User...

--- Backend/microservices/search/src/main/java/com/ms/search/controller/SearchController.java
+++ Backend/microservices/search/src/main/java/com/ms/search/controller/SearchController.java
@@ -1,23 +1,26 @@
 package com.ms.search.controller;
 
+import com.ms.search.config.CaffeineConfig;
 import com.ms.search.connectInterface.ConnectorConnect;
 import com.ms.search.connectInterface.DataConnect;
 import com.ms.search.model.SearchQuery;
+import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/search")
+@AllArgsConstructor
 public class SearchController {
 
     private final DataConnect dataConnect;
     private final ConnectorConnect connectorConnect;
-
-    public SearchController(DataConnect dataConnect, ConnectorConnect connectorConnect) {
-        this.dataConnect = dataConnect;
-        this.connectorConnect = connectorConnect;
-    }
+    private final CacheManager cacheManager;
 
     @GetMapping
     public ResponseEntity<?> getSchemas(@RequestHeader(value = "Authorization") String authorizationHeader) {
@@ -27,6 +30,16 @@ public class SearchController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping
+    @RequestMapping("/cached")
+    public Object getCache() {
+        CaffeineCache caffeineCache = (CaffeineCache) cacheManager.getCache(CaffeineConfig.CACHE_SEARCH);
+        com.github.benmanes.caffeine.cache.Cache<Object, Object> nativeCache =
+                Objects.requireNonNull(caffeineCache).getNativeCache();
+        return nativeCache.asMap().values();
+    }
+
 
     @PostMapping
     public ResponseEntity<?> search(@RequestHeader(value = "Authorization") String authorizationHeader, @RequestBody SearchQuery searchQuery) {
