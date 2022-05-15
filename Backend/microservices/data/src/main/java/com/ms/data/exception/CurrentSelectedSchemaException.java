48124f5a90808e9981804ab14cacabd39c376fbf
dkhomyk <khomyk.dima42@gmail.com>
2022-05-15 20:16:21 +0300

    added restExceptionHandler class

--- Backend/microservices/data/src/main/java/com/ms/data/exception/CurrentSelectedSchemaException.java
+++ Backend/microservices/data/src/main/java/com/ms/data/exception/CurrentSelectedSchemaException.java
@@ -3,6 +3,6 @@ package com.ms.data.exception;
 public class CurrentSelectedSchemaException extends RuntimeException {
 
     public CurrentSelectedSchemaException(String message) {
-        super();
+        super(message);
     }
 }
