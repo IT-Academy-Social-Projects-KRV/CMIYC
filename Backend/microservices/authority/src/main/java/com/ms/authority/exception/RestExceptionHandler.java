10ca3c86a73a5da2dc51c87ac3605c459588f46f
Oleh <gorbokonyk@gmail.com>
2022-05-17 21:40:39 +0300

    add @ControllerAdvice

--- Backend/microservices/authority/src/main/java/com/ms/authority/exception/RestExceptionHandler.java
+++ Backend/microservices/authority/src/main/java/com/ms/authority/exception/RestExceptionHandler.java
@@ -2,9 +2,11 @@ package com.ms.authority.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
