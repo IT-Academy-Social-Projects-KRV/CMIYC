c5608349fa3e31b0b5b77425e862aee4296f0d96
MrGold6 <75213424+MrGold6@users.noreply.github.com>
2022-05-19 13:54:02 +0300

    resolve conflict

--- Backend/microservices/authority/src/main/java/com/ms/authority/repository/UserRepository.java
+++ Backend/microservices/authority/src/main/java/com/ms/authority/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.ms.authority.repository;
 
 import com.ms.authority.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -30,5 +31,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Page<User> findUserByParams(@Param(value = "email") String email,
                                 @Param(value = "firstName") String firstName,
                                 @Param(value = "lastName") String lastName,
-                                @Param(value = "isActive") Boolean isActive);
+                                @Param(value = "isActive") Boolean isActive, Pageable pageable);
 }
