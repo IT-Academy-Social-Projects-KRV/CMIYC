7f4cbb27d6c664bac4945dd62192c5edc24e216c
Oleh <gorbokonyk@gmail.com>
2022-05-20 13:52:45 +0300

    fix qrCode view in Gmail

--- Backend/microservices/authority/src/main/java/com/ms/authority/service/EmailService.java
+++ Backend/microservices/authority/src/main/java/com/ms/authority/service/EmailService.java
@@ -2,6 +2,7 @@ package com.ms.authority.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -11,6 +12,7 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import javax.xml.bind.DatatypeConverter;
 
 @Service
 @RequiredArgsConstructor
@@ -29,12 +31,15 @@ public class EmailService {
         ctx.setVariable("link", link);
         ctx.setVariable("qrCode", qrCode);
         //TODO: replace thymeleaf to something else
+        String data = qrCode.substring(qrCode.indexOf("base64,") + 7);
+        byte[] bytes = DatatypeConverter.parseBase64Binary(data);
         final String body = this.templateEngine.process("emailConfirmation.html", ctx);
         MimeMessage mimeMessage = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
         helper.setFrom(from);
         helper.setTo(email);
         helper.setText(body, true);
+        helper.addInline("qrCode", new ByteArrayResource(bytes), "image/png");
         helper.setSubject("Confirm your email");
         emailSender.send(mimeMessage);
     }
