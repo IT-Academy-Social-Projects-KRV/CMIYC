cfe68caea1bdf7a3dc088072b021bfc98e574655
MrGold6 <75213424+MrGold6@users.noreply.github.com>
2022-05-22 01:38:07 +0300

    Feature 252 partial search

--- Frontend/src/app/admin-manage-user-list/admin-manage-user-list.component.ts
+++ Frontend/src/app/admin-manage-user-list/admin-manage-user-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClientService} from "../shared/http.client.service";
 import {User} from "../shared/data/user";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-admin-manage-user-list',
@@ -8,30 +9,65 @@ import {User} from "../shared/data/user";
   styleUrls: ['./admin-manage-user-list.component.css']
 })
 export class AdminManageUserListComponent implements OnInit {
-  idToUpdate:number|undefined;
-  firstNameToUpdate:string|undefined;
-  lastNameToUpdate:string|undefined;
-  emailToUpdate:string|undefined;
-  isUserAdminToUpdate:boolean|undefined;
-  isSchemaAdminToUpdate:boolean|undefined;
-  isUserToUpdate:boolean|undefined;
-  RESPONSE:any;
+  idToUpdate: number | undefined;
+  firstNameToUpdate: string | undefined;
+  lastNameToUpdate: string | undefined;
+  emailToUpdate: string | undefined;
+  isUserAdminToUpdate: boolean | undefined;
+  isSchemaAdminToUpdate: boolean | undefined;
+  isUserToUpdate: boolean | undefined;
+  RESPONSE: any;
   page = 1;
-  count:number|undefined;
-  tableSize=10;
+  count: number | undefined;
+  tableSize = 10;
   //tableSizes = [1, 3, 6, 9]; variable used for items per page selection
   users: User[] = [];
+
+  form: FormGroup = new FormGroup({
+    firstNameSearch: new FormControl(''),
+    lastNameSearch: new FormControl(''),
+    emailSearch: new FormControl(''),
+    isActiveSearch: new FormControl('')
+
+  });
 
   constructor(private httpClientService: HttpClientService) {
 
   }
 
+  onSubmit(): void {
+    const firstName = this.form.controls['firstNameSearch'].value;
+    const lastName = this.form.controls['lastNameSearch'].value;
+    const email = this.form.controls['emailSearch'].value;
+    const isActive = this.form.controls['isActiveSearch'].value == true;
+
+    this.httpClientService.getUsersSearchInitialPage(this.tableSize, firstName, lastName, email, isActive).subscribe({
+      next: (response) => {
+        this.RESPONSE = response;
+        this.count = this.RESPONSE.totalElements;
+        const usersList = this.RESPONSE.content;
+        this.loadUsers(usersList);
+      }
+    })
+
+  }
+
+  getAllUsersClearSearchFields(): void {
+    this.form.controls['firstNameSearch'].setValue('');
+    this.form.controls['lastNameSearch'].setValue('');
+    this.form.controls['emailSearch'].setValue('');
+    this.form.controls['isActiveSearch'].reset();
+
+    this.ngOnInit();
+  }
+
+
   ngOnInit(): void {
 
     this.httpClientService.getInitialPage(this.tableSize).subscribe({
-      next:(response)=>{
-        this.RESPONSE=response;
-        this.count=this.RESPONSE.totalElements;
+      next: (response) => {
+        this.RESPONSE = response;
+        this.count = this.RESPONSE.totalElements;
         const usersList = this.RESPONSE.content;
         this.loadUsers(usersList);
       }
@@ -40,7 +76,7 @@ export class AdminManageUserListComponent implements OnInit {
   }
 
   private loadUsers(users: User[]) {
-    this.users=[];
+    this.users = [];
     for (let i = 0; i < users.length; i++) {
       const u = users[i];
       this.users.push(
@@ -52,51 +88,69 @@ export class AdminManageUserListComponent implements OnInit {
     }
   }
 
-  onTableDataChange(event:any){
-    this.users=[];
-    this.page=event;
-    this.httpClientService.getUsersOnPage(event,this.tableSize).subscribe({
-      next:(response)=>{
-        this.RESPONSE=response;
-        const usersList = this.RESPONSE.content;
-        this.loadUsers(usersList);
-      }
-    })
+  onTableDataChange(event: any) {
+    this.users = [];
+    this.page = event;
 
+    const firstName = this.form.controls['firstNameSearch'].value;
+    const lastName = this.form.controls['lastNameSearch'].value;
+    const email = this.form.controls['emailSearch'].value;
+    const isActive = this.form.controls['isActiveSearch'].value == true;
+
+    if ((firstName == null || firstName == "") && (lastName == null || lastName == "") && (email == null || email == ""))
+      if (!isActive) {
+        this.httpClientService.getUsersOnPage(event, this.tableSize).subscribe({
+          next: (response) => {
+            this.RESPONSE = response;
+            const usersList = this.RESPONSE.content;
+            this.loadUsers(usersList);
+          }
+        })
+      } else {
+        this.httpClientService.getUsersSearchOnPage(event, this.tableSize, firstName, lastName, email, isActive).subscribe({
+          next: (response) => {
+            this.RESPONSE = response;
+            const usersList = this.RESPONSE.content;
+            this.loadUsers(usersList);
+          }
+        })
+      }
   }
-  onDelete(userId:number, userName:string, userLastName:string){
-    let message ="Are you sure you want to delete \""+userName+" "+userLastName+"\" profile?\n" +
+
+  onDelete(userId: number, userName: string, userLastName: string) {
+    let message = "Are you sure you want to delete \"" + userName + " " + userLastName + "\" profile?\n" +
       "\n" +
       "All information associated to this user profile will be permanently deleted. " +
       "This operation can not be undone.";
-    if(confirm(message)) {
-      this.httpClientService.deleteUser(userId).subscribe(list=>{
+    if (confirm(message)) {
+      this.httpClientService.deleteUser(userId).subscribe(list => {
         this.loadUsers(this.users);
       })
       window.location.reload();
     }
 
   }
-  onEdit(id:number, firstName:string, lastName:string, email:string, isUserAdmin:boolean, isSchemaAdmin:boolean, isUser:boolean){
-    this.idToUpdate=id;
-    this.firstNameToUpdate=firstName;
-    this.lastNameToUpdate=lastName;
-    this.emailToUpdate=email;
-    this.isUserAdminToUpdate=isUserAdmin;
-    this.isSchemaAdminToUpdate=isSchemaAdmin;
-    this.isUserToUpdate=isUser;
+
+  onEdit(id: number, firstName: string, lastName: string, email: string, isUserAdmin: boolean, isSchemaAdmin: boolean, isUser: boolean) {
+    this.idToUpdate = id;
+    this.firstNameToUpdate = firstName;
+    this.lastNameToUpdate = lastName;
+    this.emailToUpdate = email;
+    this.isUserAdminToUpdate = isUserAdmin;
+    this.isSchemaAdminToUpdate = isSchemaAdmin;
+    this.isUserToUpdate = isUser;
   }
 
-/*  method for items per page selection
-  onTableSizeChange(event:any): void {
-    this.tableSize = event.target.value;
-    this.page = 1;
-   // use method for display items
-  }*/
+  /*  method for items per page selection
+    onTableSizeChange(event:any): void {
+      this.tableSize = event.target.value;
+      this.page = 1;
+     // use method for display items
+    }*/
 
   onUserActiveChange(userId: number, isActive: boolean): void {
     const user: User | undefined = this.users.filter(value => value.id == userId)[0];
-    if(user && user.active != isActive) {
+    if (user && user.active != isActive) {
       this
         .httpClientService
         .setUserActive(userId, isActive)
